12535c1911a6770bed11f60beea7ee0c44175bb1
veelinktech <104140334+veelinktech@users.noreply.github.com>
2026-06-29 15:19:56 +0530

    2nd commit
    Update console logs to include operation labels

--- MFirstpgm.js
+++ MFirstpgm.js
@@ -1,4 +1,4 @@
-console.log(5+5)
-console.log(10-5)
-console.log(10*5)
-console.log(10/5)
+console.log("Add result =", 5+5)
+console.log("Sub result = ", 10-5)
+console.log("Mul result =", 10*5)
+console.log("Div result =",10/5)
