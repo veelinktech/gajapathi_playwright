c2a062f9f660bc851c483810ce55397a63c4a261
GAJAPATHI <gaja5588@gmail.com>
2026-07-02 06:22:54 -0300

    Add subtraction, multiplication, and division functions

--- MFirstpgm.js
+++ MFirstpgm.js
@@ -1,9 +1,22 @@
 function add()
 {
 console.log("Add result =", 5+5)
-console.log("Sub result = ", 10-5)
-console.log("Mul result =", 10*5)
-console.log("Div result =",10/5)
 }
-
+function sub()
+{
+  console.log("Sub result = ", 10-5)
+}
+function mul()
+{
+  console.log("Mul result =", 10*5)
+}
+function Div()
+{
+  console.log("Div result =",10/5)
+}
+  
 add()
+sub()
+mul()
+Div()
+
